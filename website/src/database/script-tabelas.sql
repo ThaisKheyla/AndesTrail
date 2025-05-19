61dad294f0fe1441dd404e93c19d30c07b65cce6
thaisKheyla <kheyla.paucara@spteh.school>
2025-05-19 10:05:41 -0300

    tudoooooooooooooooooooooooooo

---
+++ website/src/database/script-tabelas.sql
@@ -0,0 +1,93 @@
+-- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
+-- Você precisa executar os comandos no banco de dados para criar as tabelas,
+-- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+
+/*
+comandos para mysql server
+*/
+
+CREATE DATABASE aquatech;
+
+USE aquatech;
+
+CREATE TABLE empresa (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	razao_social VARCHAR(50),
+	cnpj CHAR(14),
+	codigo_ativacao VARCHAR(50)
+);
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50),
+	fk_empresa INT,
+	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+);
+
+CREATE TABLE aviso (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	titulo VARCHAR(100),
+	descricao VARCHAR(150),
+	fk_usuario INT,
+	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+);
+
+create table aquario (
+/* em nossa regra de negócio, um aquario tem apenas um sensor */
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	descricao VARCHAR(300),
+	fk_empresa INT,
+	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+);
+
+/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+
+create table medida (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	dht11_umidade DECIMAL,
+	dht11_temperatura DECIMAL,
+	luminosidade DECIMAL,
+	lm35_temperatura DECIMAL,
+	chave TINYINT,
+	momento DATETIME,
+	fk_aquario INT,
+	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
+);
+
+
+create table depoimento (
+   id_depoimento int primary key auto_increment,
+   id_usuario int,
+   id_pais int,
+   tipo varchar(10), /* 'video' ou 'audio' */
+   url_midia varchar(255),
+   descricao text,
+   data_envio datetime default current_timestamp,
+   foreign key (id_usuario) references usuario(id_usuario),
+   foreign key (id_pais) references pais(id_pais)
+);
+
+create table roteiro_viagem (
+   id_roteiro int primary key auto_increment,
+   id_usuario int,
+   nome_roteiro varchar(100),
+   data_criacao datetime default current_timestamp,
+   foreign key (id_usuario) references usuario(id_usuario)
+);
+
+create table destino_roteiro (
+   id_destino int primary key auto_increment,
+   id_roteiro int,
+   id_pais int,
+   observacoes text,
+   ordem_visita int,
+   foreign key (id_roteiro) references roteiro_viagem(id_roteiro),
+   foreign key (id_pais) references pais(id_pais)
+);
+
+insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
+insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
+insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
+insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
